09972484edb919198d7ada2934a77fce68c78872
Chelsea Merrill <chelmer1223@aol.com>
2021-06-15 13:37:33 -0400

    changed layout

--- src/Components/Navigation/navigation.component.jsx
+++ src/Components/Navigation/navigation.component.jsx
@@ -6,7 +6,7 @@ import './navigation.css';
 const Navigation = () => (
     <nav>
       <ul>
-        <p><NavLink to='/'>Home</NavLink></p>
+        <p><NavLink to='/'>About Me</NavLink></p>
         <p><NavLink to='/'>Projects</NavLink></p>
         <p><NavLink to='/'>Resume</NavLink></p>
       </ul>

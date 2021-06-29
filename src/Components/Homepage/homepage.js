3c363e5d0eaca5fecc54fde3e57a98031f067169
Chelsea Merrill <chelmer1223@aol.com>
2021-06-28 20:58:33 -0400

    fix: display components

--- src/Components/Homepage/homepage.js
+++ src/Components/Homepage/homepage.js
@@ -1,10 +1,17 @@
 import React, {Component} from 'react';
+import About from '../About Me/About';
+import Projects from '../Projects/projects';
+import Res from '../Resume/resume';
+import Landing from '../LandingPage/landingpage';
 
 
 function Homepage (){
         return(
             <div className='homepage'>
-
+                <Landing/>
+                <About/>
+                <Projects/>
+                <Res/>
             </div>
         )
 };

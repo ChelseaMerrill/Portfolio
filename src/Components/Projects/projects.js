61fcc5035d1ca2ecccc8037804c1bf3d96b5b394
Chelsea Merrill <chelmer1223@aol.com>
2021-07-08 18:52:58 -0400

    updated projects

--- src/Components/Projects/projects.js
+++ src/Components/Projects/projects.js
@@ -9,13 +9,13 @@ class Projects extends Component {
         this.state = {
             projects: [
                 {
-                    name: 'calculator',
+                    name: 'Calculator',
                     description: 'Using DOM manipulation we were instructed to design a functioning mathmatical calculator without using the built in method.  Due to this being a basic, four function calculator, I wanted to design my calculator in a way that was attractive to the users age group.  I drew my own backgrounds and image to give my calculator the look of a crayola box.  I created my own curser as a blank crayon and then designed a paint pallet so that the user could change the color of te curser which, in turn, changes the color of the screen text',
                     image: 'image',
                     link: 'https://chelseamerrill.github.io/Calculator/',
                 },
                 {
-                    name: 'NotNetflix',
+                    name: 'Not Netflix',
                     description: 'Using React App and a movie based API I crated a site that mimiked the concept of Netflix.  Being as this was a project in which several people were completing the same project I wanted to come up with a project that really contrasted the redunent Netflix look that was going to be presented several times.  I decided to brand my site on what I belived Blockbuster would have looked like if streaming platforms existed during the years that Blockbuster was at its prime.  To do this i focused on design aethetics that haad an early 2000s vibe and imbedded Youtube videos to act as a priview ',
                     image: 'image',
                     link: 'http://google.com',
@@ -33,7 +33,13 @@ class Projects extends Component {
         return (
             <div className='projects'>
                 <h1>Projects</h1>
-
+                {this.state.projects.map((project) => (
+                    <div key={project.name}>
+                        <h2>{project.name}</h2>
+                        <p>{project.description}</p>
+                        <p>{project.link}</p>
+                    </div>
+                ))}
 
                 <button type="button" onClick={(e) => { e.preventDefault(); window.location.href = 'http://thebookbelles.com'; }}>
 

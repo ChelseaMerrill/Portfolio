109dca9c2a45b4d1d1831b376e631a16f5756c53
Chelsea Merrill <chelmer1223@aol.com>
2022-02-12 13:26:15 -0500

    fix: routes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import FullPage from './Components/FullPage/fullpage';
 import Navigation from './Components/Navigation/navigatigation';
-import About from './Components/About Me/About';
-import Projects from './Components/Projects/projects';
-import Res from './Components/Resume/resume';
-import Landing from './Components/LandingPage/landingPage';
+// import About from './Components/About Me/About';
+// import Projects from './Components/Projects/projects';
+// import Res from './Components/Resume/resume';
+// import Landing from './Components/LandingPage/landingPage';
 
 import './App.css';
 
 function App() {
   return (
     <div className='App'>
-            <Router>
-              <Navigation/>
-                  <Switch>
-                    <Route exact path='/' component={FullPage}/>
-                    <Route exact path='/landing' component={Landing}/>
-                    <Route exact path='/about' component={About}/>
-                    <Route exact path='/projects' component={Projects}/>
-                    <Route exact path='./resume' component={Res}/>
-                  </Switch>
-            </Router>
+      <Navigation/>
+      <div className='myPages'>
+        <FullPage/>
+        {/* <Landing/>
+        <About/>
+        <Projects/>
+        <Res/> */}
+      </div>
     </div>
   );
 }
--- src/Components/LandingPage/landingpage.js
+++ src/Components/LandingPage/landingpage.js
@@ -5,18 +5,19 @@ import './landingPage.css';
 
 function Landing (){
         return(
-            <div className='typist'>
-                <h1>Hello,</h1>  
-                <Typist>       
+            <div className='typist page'> 
+                <Typist>
+                    <h1>Hello,</h1>  
+                    <Typist.Delay ms={2000}/>     
                     <h3>I'm</h3>
                     <Typist.Delay ms={2000}/>
-                    <h3>~ a creator</h3>
+                    <h3>a creator</h3>
                     <Typist.Delay ms={2000}/>
-                    <h3>~ a designer</h3>
+                    <h3>a designer</h3>
                     <Typist.Delay ms={2000}/>
-                    <h3>~ more than your average Software Engineer</h3>
+                    <h3>more than your average Software Engineer</h3>
                     <Typist.Delay ms={2000}/>
-                    <h1>Chelsea!</h1>
+                    <h1>Chelsea</h1>
                 </Typist>
             </div>
         )
